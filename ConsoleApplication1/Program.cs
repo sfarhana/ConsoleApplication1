00e371772d6aaebcfe2210e1c69be66c3ab7be24
sfarhana <fsultanacse@gmail.com>
2019-09-10 21:10:40 +0600

    All files of assignment 1 uploaded

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Enter your name please:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Address:");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter your Age:");
+            string Age = Console.ReadLine();
+            Console.WriteLine("your Contact:");
+            string Contact = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine+"Here is your information:" + Environment.NewLine + 
+                "Name:" + name + Environment.NewLine + "Address:" + Address + Environment.NewLine + 
+                "Age:" + Age+ Environment.NewLine + "Contact no:" + Contact);
+                               
+            Console.ReadKey();
+
+        }
+    }
+}
